c4ff65527bf9bfe8cf1f80413a6a94523fc41d75
Atunbi Christianah Ifeoluwa <ifeoluwaatunbi17@gmail.com>
2022-06-19 01:49:24 +0100

    commit on main

--- christydjangoproj/blog/models.py
+++ christydjangoproj/blog/models.py
@@ -1,3 +1,11 @@
+from django.conf import settings
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+class blogpost(models.Model):
+    title   =models.CharField(max_length=200)
+    body    =models.TextField(max_length=5000, null=False, blank=False)
+    Author  =models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    Created_date =models.DateTimeField(auto_now=True, verbose_name='date_created')
+    Published_date =models.DateTimeField(auto_now_add=True, verbose_name='date_published')
+
